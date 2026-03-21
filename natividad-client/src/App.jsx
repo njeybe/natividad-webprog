2a7e2abfb0f1ba7bdf94036091b9714293928672
JB Natividad <natividadjosephbrian@gmail.com>
2026-03-21 09:51:21 +0800

    use prettier for clean code format

--- natividad-client/src/App.jsx
+++ natividad-client/src/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div className='App'>
-        <header className='App-header'>
+      <div className="App">
+        <header className="App-header">
           <h1>Welcome to My React App!</h1>
           <p>
-            Name: Joseph Brian Natividad <br/>
-            Email: natividadjd@students.national-u.edu.ph <br/>
-            Other Personal Info: 3rd Year BSIT Student <br/>
-            <a href="https://github.com/njeybe/natividad-webprog">GitHub Repository</a>
+            Name: Joseph Brian Natividad <br />
+            Email: natividadjd@students.national-u.edu.ph <br />
+            Other Personal Info: 3rd Year BSIT Student <br />
+            <a href="https://github.com/njeybe/natividad-webprog">
+              GitHub Repository
+            </a>
           </p>
-          
         </header>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
