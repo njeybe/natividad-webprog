1c90f1826fd64eeb0b98372705b4c6f328c035f0
Joseph Brian Natividad <natividadjosephbrian@gmail.com>
2026-03-27 21:43:08 +0800

    Revert "done with boilerplates for components, pages, and App.jsx"
    This reverts commit 211984a8447e61b718818884da71d22357e9a8a7.

--- natividad-client/src/App.jsx
+++ natividad-client/src/App.jsx
@@ -1,36 +1,24 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./components/Layout";
-import ArticlePage from "./pages/ArticlePage";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
+import React from "react";
+import "./App.css";
 
-const routes = [
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <HomePage />,
-      },
-      {
-        path: "about",
-        element: <AboutPage />,
-      },
-      {
-        path: "article",
-        element: <ArticlePage />,
-      },
-    ],
-  },
-];
-
-const router = createBrowserRouter(routes);
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My React App!</h1>
+          <p>
+            Name: Joseph Brian Natividad <br />
+            Email: natividadjd@students.national-u.edu.ph <br />
+            Other Personal Info: 3rd Year BSIT Student <br />
+            <a href="https://github.com/njeybe/natividad-webprog">
+              GitHub Repository
+            </a>
+          </p>
+        </header>
+      </div>
     </>
   );
 }
+
 export default App;
